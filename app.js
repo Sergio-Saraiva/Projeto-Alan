6adc4e537cdc3b26827aca52dc9fd52dd5da2f5c
Sergio-Saraiva <38301294+Sergio-Saraiva@users.noreply.github.com>
2019-12-11 12:06:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -75,11 +75,17 @@ addtelefone.addEventListener("click", function () {
     $('#telefone-div').append('<div id="telefone-div" class="form-group col-md-6">    <label for="telefone">Telefone</label></i><i id="subtelefone" class="far fa-minus-square"></i>    <input class="form-control phone_with_ddd" type="text" id="telefone" name="telefone"  placeholder="(__) ____-____"></div>');
     subtelefone = document.getElementById('subtelefone');
     subtelefone.addEventListener("click", function () {
-    $('#subtelefone').unbind("click");
-    $('#subtelefone').bind("click", function () {
-        $('#subtelefone').parent().remove();     
-    })
-   
+    $('#subtelefone').parent().remove();
 })
 });
+
+addtelefone = document.getElementById('addtelefone');
+addtelefone.addEventListener("click", function () {
+    var div = document.createElement('div');
+    div.setAttribute = ('id', 'telefone-div');
+    div.setAttribute = ('class', 'form-group col-md-6');
+    
+    var label = document.createElement()
+
+})
 
