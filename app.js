3868f195682ff0f3dd618f720e5225291e4883b1
Sergio-Saraiva <38301294+Sergio-Saraiva@users.noreply.github.com>
2019-12-12 17:14:55 -0300

    Função de adicionar campos de endereço adicionada

--- app.js
+++ app.js
@@ -20,7 +20,30 @@ hcnpj.addEventListener("blur", function () {
     }
 });  
 
-//mostrar dados recebidos da api
+
+axios.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(function (response) {
+    console.log(response);
+    renderElementEstado(response);
+}).catch(function (error) {
+    console.log(error);
+});
+
+//mostrar dados recebidos da api de estados
+function renderElementEstado(json) {
+    var select = document.getElementById('estado');
+    var tam = json.data.length;
+
+    for(i = 0; i<tam; i++){
+        var option = document.createElement('option');
+        option.value = json.data[i].sigla;
+        option.innerHTML = json.data[i].nome;
+        select.appendChild(option);    
+    }
+    
+}
+
+
+//mostrar dados recebidos da api de cnpj
 function renderElement(json) {
     var status = json.data.status; 
     if( status == "OK" ){
@@ -118,12 +141,16 @@ subtelefone.addEventListener("click", function () {
 var e = 1;
 var addendereco = document.getElementById('addendereco');
 addendereco.addEventListener("click", function () {
+   e++;
    var divEnd = document.getElementById('divEnd');
+   
+   //cria elemento de nome
    var div = document.createElement('div');
    div.setAttribute('class', 'form-group');
+   div.setAttribute('id', 'div'+e);
    var label = document.createElement('label');
    label.setAttribute('for', 'nome');
-   label.innerHTML = 'Nome';
+   label.innerHTML = 'Nome '+e;
    var input = document.createElement('input');
    input.setAttribute('class', 'form-control');
    input.setAttribute('type', 'text');
@@ -134,8 +161,11 @@ addendereco.addEventListener("click", function () {
    div.appendChild(label);
    div.appendChild(input);
 
+
+   //cira elementos de logradouro numero e bairro
    div2 = document.createElement('div')
    div2.setAttribute('class', 'form-row');
+   div2.setAttribute('id', 'div2'+e);
    var divlog = document.createElement('div');
    divlog.setAttribute('class', 'form-group col-md-6');
    var divN = document.createElement('div');
@@ -143,26 +173,115 @@ addendereco.addEventListener("click", function () {
    var divB = document.createElement('div'); 
    divB.setAttribute('class', 'form-group col-md-4');
    
-    var labelLog = document.createElement('label');
-    labelLog.setAttribute('for','logradouro');
-    labelLog.innerHTML = 'Logradouro';
+   var labelLog = document.createElement('label');
+   labelLog.setAttribute('for','logradouro');
+   labelLog.innerHTML = 'Logradouro '+e;
 
-    var inputLog = document.createElement('input');
-    inputLog.setAttribute('class', 'form-control');
+   var inputLog = document.createElement('input');
+   inputLog.setAttribute('class', 'form-control');
    inputLog.setAttribute('type', 'text');
    inputLog.setAttribute('name', 'logradouro[]');
    inputLog.setAttribute('id', 'logradouro');
+   
+   var labelN = document.createElement('label');
+   labelN.setAttribute('for','numero');
+   labelN.innerHTML = 'Nº '+e;
+
+   var inputN = document.createElement('input');
+   inputN.setAttribute('class', 'form-control');
+   inputN.setAttribute('type', 'number');
+   inputN.setAttribute('name', 'numero[]');
+   inputN.setAttribute('id', 'numero');
+
+   var labelB = document.createElement('label');
+   labelB.setAttribute('for','bairro');
+   labelB.innerHTML = 'Bairro '+e;
+
+   var inputB = document.createElement('input');
+   inputB.setAttribute('class', 'form-control');
+   inputB.setAttribute('type', 'text');
+   inputB.setAttribute('name', 'bairro[]');
+   inputB.setAttribute('id', 'bairro');
 
 
    divEnd.appendChild(div2);
    div2.appendChild(divlog);
    divlog.appendChild(labelLog);
-   labelLog.appendChild(inputLog);
+   divlog.appendChild(inputLog);
    div2.appendChild(divN);
+   divN.appendChild(labelN);
+   divN.appendChild(inputN);
    div2.appendChild(divB);
+   divB.appendChild(labelB);
+   divB.appendChild(inputB);
 
-   
+    //cria elementos de cidade, estado e cep
+    var div3 = document.createElement('div');
+    div3.setAttribute('class', 'form-row');
+    div3.setAttribute('id', 'div3'+e);
+    var divC = document.createElement('div');
+    divC.setAttribute('class', 'form-group col-md-4');
+    var divE = document.createElement('div');
+    divE.setAttribute('class', 'form-group col-md-4');
+    var divCep = document.createElement('div');
+    divCep.setAttribute('class', 'form-group col-md-4');
+
+    var labelC = document.createElement('label');
+    labelC.setAttribute('for','cidade');
+    labelC.innerHTML = 'Cidade '+e;
+
+    var inputC = document.createElement('input');
+    inputC.setAttribute('class', 'form-control');
+    inputC.setAttribute('type', 'text');
+    inputC.setAttribute('name', 'cidade[]');
+    inputC.setAttribute('id', 'cidade');
+
+    var labelE = document.createElement('label');
+    labelE.setAttribute('for','estado');
+    labelE.innerHTML = 'Estado '+e;
+
+    var selectE = document.createElement('select');
+    selectE.setAttribute('class', 'custom-select');
+    selectE.setAttribute('name', 'estado[]');
+    selectE.setAttribute('id', 'estado');
+
+    var labelCep = document.createElement('label');
+    labelCep.setAttribute('for','cep');
+    labelCep.innerHTML = 'CEP '+e;
+
+    var inputCep = document.createElement('input');
+    inputCep.setAttribute('class', 'form-control');
+    inputCep.setAttribute('type', 'number');
+    inputCep.setAttribute('name', 'cep[]');
+    inputCep.setAttribute('id', 'cep');
+
+    divEnd.appendChild(div3);
+    div3.appendChild(divC);
+    divC.appendChild(labelC);
+    divC.appendChild(inputC);
+
+    div3.appendChild(divE)
+    divE.appendChild(labelE);
+    divE.appendChild(selectE);
+
+    div3.appendChild(divCep);
+    divCep.appendChild(labelCep);
+    divCep.appendChild(inputCep);
+
+    renderElementEstado();   
 });
+
+var subendereco = document.getElementById('subendereco');
+subendereco.addEventListener("click", function () {
+    var div = document.getElementById('div'+e);
+    var div2 = document.getElementById('div2'+e);
+    var div3 = document.getElementById('div3'+e);
+    
+    div.parentNode.removeChild(div);
+    div2.parentNode.removeChild(div2);
+    div3.parentNode.removeChild(div3);
+    e--;
+})
 
 // var addendereco = document.getElementById('addendereco');
 // addendereco.addEventListener("click", function () {
--- registro-pessoa.php
+++ registro-pessoa.php
@@ -90,33 +90,7 @@ if($v == 1){
                     <label for="estado">Estado</label>
                     <select id="estado" class="custom-select" name="estado[]">
                         <option selected>Selecione o estado</option>
-                        <option value="AC">Acre</option>
-                        <option value="AL">Alagoas</option>
-                        <option value="AP">Amapá</option>
-                        <option value="AM">Amazonas</option>
-                        <option value="BA">Bahia</option>
-                        <option value="CE">Ceará</option>
-                        <option value="DF">Distrito Federal</option>
-                        <option value="ES">Espírito Santo</option>
-                        <option value="GO">Goiás</option>
-                        <option value="MA">Maranhão</option>
-                        <option value="MT">Mato Grosso</option>
-                        <option value="MT">Mato Grosso do Sul</option>
-                        <option value="MG">Minas Gerais</option>
-                        <option value="PA">Pará</option>
-                        <option value="PB">Paraíba</option>
-                        <option value="PR">Paraná</option>
-                        <option value="PB">Pernambuco</option>
-                        <option value="PI">Piauí</option>
-                        <option value="RJ">Rio de Janeiro</option>
-                        <option value="RN">Rio Grande do Norte</option>
-                        <option value="RS">Rio Grande do Sul</option>
-                        <option value="RO">Rondônia</option>
-                        <option value="RR">Roraima</option>
-                        <option value="SC">Santa Catarina</option>
-                        <option value="SP">São Paulo</option>
-                        <option value="SE">Sergipe</option>
-                        <option value="TO">Tocatins</option>
+                        
                     </select>
                 </div>
                 <div class="form-group col-md-4">
